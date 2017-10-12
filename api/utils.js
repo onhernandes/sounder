3887c4574e827ba8ffd3e8c485639a0bca4904c6
Matheus Hernandes <midia.matheus@gmail.com>
2017-10-12 13:52:45 -0300

    add utils.js

---
+++ api/utils.js
@@ -0,0 +1,8 @@
+var fs = require('fs'),
+	path = require('path'),
+	logger = require('./logger.js'),
+	Music = require('./schema.js');
+
+function deleteMusic(music) {
+	return new Promise((resolve, reject) => {});
+}
