86c3cbd3d1b4e3a831c6428bc350bc89511af363
Kaio Teixeira <kaio.teco@gmail.com>
2018-03-29 15:26:50 -0300

    routes

--- api/routes.js
+++ api/routes.js
@@ -1,5 +1,7 @@
-const music = require('./music/route.js')
+const music = require('./models/music/route.js')
+const user = require('./models/user/route.js')
 
 module.exports = {
-  '/api/music': music
+    '/api/music': music,
+    '/api/user': user
 }
